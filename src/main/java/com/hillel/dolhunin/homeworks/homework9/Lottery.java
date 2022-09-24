0786ee8ab707092176c240556243718a021d24d4
Dolhunin Maksym <maksdolhunin@gmail.com>
2022-09-24 21:46:16 +0300

    lottery

--- src/main/java/com/hillel/dolhunin/homeworks/homework9/Lottery.java
+++ src/main/java/com/hillel/dolhunin/homeworks/homework9/Lottery.java
@@ -13,50 +13,27 @@ public class Lottery {
         for (int i = 0; i < lotteryArray.length; i++) {
             lotteryArray[i] = (int) (Math.random() * 10);
         }
-        System.out.println(Arrays.toString(lotteryArray));
 
         for (int i = 0; i < userArray.length; i++) {
             userArray[i] = (int) (Math.random() * 10);
         }
-        System.out.println(Arrays.toString(userArray));
-
-        System.out.println();
 
         Arrays.sort(lotteryArray);
         Arrays.sort(userArray);
 
         System.out.println(Arrays.toString(lotteryArray));
         System.out.println(Arrays.toString(userArray));
+        System.out.println();
 
-        if(lotteryArray[0] == userArray[0]) {
-            System.out.println("Совпадение в первой ячейке!");
-            counter++;
-        }
-        if(lotteryArray[1] == userArray[1]) {
-            System.out.println("Совпадение во второй ячейке!");
-            counter++;
-        }
-        if(lotteryArray[2] == userArray[2]) {
-            System.out.println("Совпадение в третьей ячейке!");
-            counter++;
-        }
-        if(lotteryArray[3] == userArray[3]) {
-            System.out.println("Совпадение в четвертой ячейке!");
-            counter++;
-        }
-        if(lotteryArray[4] == userArray[4]) {
-            System.out.println("Совпадение в пятой ячейке!");
-            counter++;
-        }
-        if(lotteryArray[5] == userArray[5]) {
-            System.out.println("Совпадение в шестой ячейке!");
-            counter++;
-        }
-        if(lotteryArray[6] == userArray[6]) {
-            System.out.println("Совпадение в седьмой ячейке!");
-            counter++;
+        for (int i = 0; i < N; i++) {
+            if(lotteryArray[i] == userArray[i]) {
+                counter += 1;
+                System.out.println("Збіги у комірці - " + (i + 1));
+            }
         }
         System.out.println();
-        System.out.println("Всего совпадений - " + counter);
+        System.out.println("Кількість збігів: " + counter);
     }
 }
+
+
