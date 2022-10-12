16a0448a479008b87d26d7a55732c411aa350767
Dolhunin Maksym <maksdolhunin@gmail.com>
2022-10-12 16:54:08 +0300

    matrix

--- src/main/java/com/hillel/dolhunin/homeworks/homework10/Matrix.java
+++ src/main/java/com/hillel/dolhunin/homeworks/homework10/Matrix.java
@@ -5,55 +5,60 @@ import java.util.Scanner;
 public class Matrix {
     public static void main(String[] args) {
 
-        System.out.println("Введіть два числа:");
         Scanner scanner = new Scanner(System.in);
 
-        if (scanner.hasNextInt()) {
-            int m = scanner.nextInt();
-            scanner.nextLine();
-            if (scanner.hasNextInt()) {
-                int n = scanner.nextInt();
-                scanner.nextLine();
+        int n = getNumber(scanner);
+        int m = getNumber(scanner);
 
-                if ((m > 0) && (n > 0)) {
+        int[][] array1 = new int[m][n];
+        int[][] array2 = new int[n][m];
 
-                    int[][] array1 = new int[m][n];
-                    int[][] array2 = new int[n][m];
-
-                    for (int i = 0; i < array1.length; i++) {
-                        for (int j = 0; j < array1[i].length; j++) {
-                            array1[i][j] = (int) (Math.random() * 101);
-                        }
-                    }
-                    for (int i = 0; i < array1.length; i++) {
-                        for (int j = 0; j < array1[i].length; j++) {
-                            System.out.print(array1[i][j] + "\t");
-                        }
-                        System.out.println();
-                    }
-                    System.out.println();
-
-                    for (int i = 0; i < array2.length; i++) {
-                        for (int j = 0; j < array2[i].length; j++) {
-                            array2[i][j] = (int) (Math.random() * 101);
-                        }
-                    }
-
-                    for (int i = 0; i < array2.length; i++) {
-                        for (int j = 0; j < array2[i].length; j++) {
-                            System.out.print(array2[i][j] + "\t");
-                        }
-                        System.out.println();
-                    }
-                } else {
-                    System.out.println("Помилка! Введіть два числа більше нуля!");
-                }
-            } else {
-                System.out.println("Введіть два числа!");
+        for (int i = 0; i < array1.length; i++) {
+            for (int j = 0; j < array1[i].length; j++) {
+                array1[i][j] = (int) (Math.random() * 101);
             }
-        } else {
-            System.out.println("Помилка! Введіть число!");
         }
+        System.out.println("First array:");
+        printArray(array1);
+
+        for (int i = 0; i < array2.length; i++) {
+            for (int j = 0; j < array2[i].length; j++) {
+                array2[i][j] = array1[j][i];
+            }
+        }
+        System.out.println("Second array3:");
+        printArray(array2);
+
+        scanner.close();
+    }
+
+    static void printArray(int[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+
+    static int getNumber(Scanner scanner) {
+        int temp = 0;
+        System.out.println("Please enter array length");
+        while (true) {
+            if (scanner.hasNextInt()) {
+                temp = scanner.nextInt();
+                if (temp < 0) {
+                    System.out.println("Please enter positive number");
+                } else {
+                    break;
+                }
+                scanner.nextLine();
+            } else {
+                System.out.println("Wrong data, try again");
+                scanner.nextLine();
+            }
+        }
+        return temp;
     }
 }
 
--- src/main/java/com/hillel/dolhunin/homeworks/homework14/PopMusic.java
+++ src/main/java/com/hillel/dolhunin/homeworks/homework14/PopMusic.java
@@ -1,7 +1,6 @@
 package com.hillel.dolhunin.homeworks.homework14;
 
 public class PopMusic extends MusicStyles{
-
     @Override
     void playMusic() {
         System.out.println("Play pop music");
